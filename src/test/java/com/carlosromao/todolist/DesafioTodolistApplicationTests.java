437540da4dcaf06b5ab0daa81d18274bfddbd590
carl0sR0ma0 <cvpromao@gmail.com>
2024-12-18 23:20:19 -0300

    :tada: Iniciando o projeto todolist e criando as pastas para arquitetura limpa

---
+++ src/test/java/com/carlosromao/todolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosromao.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
